85bce5410cdfa57380b3218e99d3a04bff645b15
Rajab <rkarume@gmail.com>
2022-08-29 18:17:25 +0300

    commit tree ecff2d1e2d3aaeda8d465d11ff37ee36766af3f7

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => console.log(json))
+    
+  
+  
 }
 
 function renderBooks(books) {
@@ -14,4 +20,7 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  // renderBooks(data);
+  
+  
 });
